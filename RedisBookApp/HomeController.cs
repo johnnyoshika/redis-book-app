f53f9cbfce06469a186d64765314fcb6963b3156
Johnny Oshika <johnnyoshika@gmail.com>
2018-04-05 19:18:27 -0700

    Add default route.

---
+++ RedisBookApp/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RedisBookApp
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index() =>
+            Content("Hello world!");
+    }
+}
